27fe1bede9f2080f0135df2500538c44c1df3399
khzeng <jacky55062003@gmail.com>
2020-04-17 23:38:33 -0700

    minor edit in network

--- network/network.py
+++ network/network.py
@@ -10,15 +10,17 @@ class model_CNN(nn.Module):
         super(model_CNN, self).__init__()
         # some hypeparameters
         self.cfg = cfg
+        self.batch_size = cfg.model.batch_size
+        self.input_dim = cfg.model.input_dim
 
         # allocate model parameters
         self.backbone = protocol[cfg.model.protocol](num_classes=cfg.model.hidden_size)
-        self.fc = nn.Linear(cfg.hidden_size * 2, cfg.output_size)
+        self.fc = nn.Linear(cfg.model.hidden_size * 2, cfg.model.output_size)
         self.tanh = nn.Tanh()
 
     def forward(self, x):
         # resort the input dimension
-        x = x.view(self.batch_size, self.input_num_frame, self.input_size, self.input_size)
+        x = x.view(self.batch_size, self.input_dim[0], self.input_dim[1], self.input_dim[2])
 
         # do forward through CNN
         x = self.tanh(self.backbone(x))
